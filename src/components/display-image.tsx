8bb2e97046211eb8ab45a393f32e69a225a51114
Comelli <comelligg@gmail.com>
2024-03-30 11:23:06 -0300

    feat: add display-image component

---
+++ src/components/display-image.tsx
@@ -0,0 +1,19 @@
+import '../../public/Outdoor.png'
+import '../../public/LogoSlogan.png'
+
+interface DisplayImageProps {
+  type: "logo" | "outdoor"
+}
+
+function DisplayImage({ type }: DisplayImageProps) {
+  switch (type) {
+    case "logo": {
+      return <img src="LogoSlogan.png" alt="Payper: Sua folha em boas mãos."></img>
+    }
+    case "outdoor": {
+      return <img src="Outdoor.png" alt="Payper Outdoor" className='size-full'></img>
+    }
+  }
+}
+
+export default DisplayImage
