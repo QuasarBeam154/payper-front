c4b63c80ff5c20f00723de626a9dfb3c722333f6
Comelli <comelligg@gmail.com>
2024-04-01 22:29:09 -0300

    refactor: optimize tailwind

--- src/components/buttons/primary-button.tsx
+++ src/components/buttons/primary-button.tsx
@@ -5,7 +5,7 @@ interface PrimaryButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   children: ReactNode
 }
 
-function PrimaryButton({ label, children, ...rest }: PrimaryButtonProps) {
+const PrimaryButton = ({ label, children, ...rest }: PrimaryButtonProps) => {
   return (
     <button {...rest} className="flex justify-center items-center w-fit h-fit px-12 py-5 gap-6 rounded-3xl outline-none select-none bg-blue-600 transition-colors ease-in-out duration-200
                                hover:bg-blue-500 active:bg-blue-700 focus:bg-blue-700 focus:ring-2 focus:ring-blue-200">
