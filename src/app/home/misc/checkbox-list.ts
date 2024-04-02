bec336be800f25137d5befdcf322fdbd9c43a0c3
Comelli <comelligg@gmail.com>
2024-04-01 22:28:17 -0300

    feat: add checkbox arraylist

---
+++ src/app/home/misc/checkbox-list.ts
@@ -0,0 +1,77 @@
+
+const checkboxList = [
+  {
+    label: 'FGTS',
+    id: '1',
+    name: 'fgts',
+    checked: false
+  },
+  {
+    label: 'Vale Alimentação',
+    id: '2',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+  {
+    label: '',
+    id: '',
+    name: '',
+    checked: false
+  },
+]
+
+export default checkboxList
