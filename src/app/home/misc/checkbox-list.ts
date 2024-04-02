e261d99d757b4154175dc637bf0ae5ddf7557c97
Comelli <comelligg@gmail.com>
2024-04-02 19:28:46 -0300

    feat: add checkboxes

--- src/app/home/misc/checkbox-list.ts
+++ src/app/home/misc/checkbox-list.ts
@@ -2,74 +2,74 @@
 const checkboxList = [
   {
     label: 'FGTS',
-    id: '1',
+    id: '01',
     name: 'fgts',
     checked: false
   },
   {
     label: 'Vale Alimentação',
-    id: '2',
-    name: '',
+    id: '02',
+    name: 'food_voucher',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Vale Transporte',
+    id: '03',
+    name: 'transport_voucher',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Imposto de Renda Retido na Fonte',
+    id: '04',
+    name: 'irrf',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Descanso Semanal Remunerado',
+    id: '05',
+    name: 'dsr',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Adicional Noturno',
+    id: '06',
+    name: 'an',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Adicional por Tempo de Serviço',
+    id: '07',
+    name: 'ats',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Diária para Viagens',
+    id: '08',
+    name: 'travel_allowance',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Insalubridade',
+    id: '09',
+    name: 'insalubrity',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Periculosidade',
+    id: '10',
+    name: 'dangerousness',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Auxílio Babá',
+    id: '11',
+    name: 'babysitter_assist',
     checked: false
   },
   {
-    label: '',
-    id: '',
-    name: '',
+    label: 'Salário Família',
+    id: '12',
+    name: 'family_wage',
     checked: false
   },
 ]
