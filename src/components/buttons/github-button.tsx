22d9398232455e8efe2a5fe4185cae04da50396f
Comelli <comelligg@gmail.com>
2024-04-01 22:29:04 -0300

    refactor: optimize tailwind

--- src/components/buttons/github-button.tsx
+++ src/components/buttons/github-button.tsx
@@ -5,10 +5,10 @@ interface GithubButtonProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
   children: ReactNode
 }
 
-function GithubButton({ label, children, ...rest }: GithubButtonProps) {
+const GithubButton = ({ label, children, ...rest }: GithubButtonProps) => {
   return (
     <a {...rest} className="flex justify-center items-center w-fit h-fit px-12 py-5 gap-6 rounded-3xl outline-none select-none bg-slate-700 transition-colors ease-in-out duration-200
-                               hover:bg-slate-600 active:bg-slate-800 focus:bg-slate-800 focus:ring-2 focus:ring-slate-200">
+                          hover:bg-slate-600 active:bg-slate-800 focus:bg-slate-800 focus:ring-2 focus:ring-slate-200">
       <h1 className="text-[64px] font-semibold text-slate-50">{ label }</h1>
       {children}
     </a>
