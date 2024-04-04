e6a514601a72e2c1bfb01ee7e9945ff98253f02d
Comelli <comelligg@gmail.com>
2024-04-03 22:09:32 -0300

    refactor: remove unused checkboxes

--- src/app/home/misc/checkbox-list.ts
+++ src/app/home/misc/checkbox-list.ts
@@ -1,12 +1,6 @@
 
 const checkboxList = [
   {
-    label: 'FGTS',
-    id: '01',
-    name: 'fgts',
-    checked: false
-  },
-  {
     label: 'Vale Alimentação',
     id: '02',
     name: 'food_voucher',
@@ -19,33 +13,9 @@ const checkboxList = [
     checked: false
   },
   {
-    label: 'Imposto de Renda Retido na Fonte',
-    id: '04',
-    name: 'irrf',
-    checked: false
-  },
-  {
-    label: 'Descanso Semanal Remunerado',
-    id: '05',
-    name: 'dsr',
-    checked: false
-  },
-  {
     label: 'Adicional Noturno',
     id: '06',
     name: 'an',
-    checked: false
-  },
-  {
-    label: 'Adicional por Tempo de Serviço',
-    id: '07',
-    name: 'ats',
-    checked: false
-  },
-  {
-    label: 'Diária para Viagens',
-    id: '08',
-    name: 'travel_allowance',
     checked: false
   },
   {
@@ -60,18 +30,7 @@ const checkboxList = [
     name: 'dangerousness',
     checked: false
   },
-  {
-    label: 'Auxílio Babá',
-    id: '11',
-    name: 'babysitter_assist',
-    checked: false
-  },
-  {
-    label: 'Salário Família',
-    id: '12',
-    name: 'family_wage',
-    checked: false
-  },
+
 ]
 
 export default checkboxList
